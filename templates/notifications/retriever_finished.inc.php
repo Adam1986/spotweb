53d0b81750d71ee8da87232dacdb6f92339c6d29
Adam1986 <Adam1986@users.noreply.github.com>
2017-11-16 14:52:46 +0000

    Update retriever_finished.inc.php

--- templates/notifications/retriever_finished.inc.php
+++ templates/notifications/retriever_finished.inc.php
@@ -3,12 +3,12 @@ New retrieved spots
 There <?php
 echo ($newSpotCount == 1) ? "is " . $newSpotCount . " spot" : "are " . $newSpotCount . " spots";
 if ($newCommentCount > 0) {
-	echo ($newReportCount > 0) ? ", " : " en ";
+	echo ($newReportCount > 0) ? ", " : " and ";
 	echo $newCommentCount;
 	echo ($newCommentCount == 1) ? " comment" : " comments";
 } # if
 if ($newReportCount > 0) {
-	echo " en " . $newReportCount;
+	echo " and " . $newReportCount;
 	echo ($newCommentCount == 1) ? " reports" : " reports";
 } # if
 ?> retrieving.
